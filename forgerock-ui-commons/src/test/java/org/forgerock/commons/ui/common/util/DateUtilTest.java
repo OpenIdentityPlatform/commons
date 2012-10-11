4f270e608d1c97869f770061a11feccf4268a82a
Jake Feasel <jake.feasel@forgerock.com>
2012-10-11 22:32:41 +0000

    Temporarily disabling jstestrunner dependency, until we can get phantomjs in jenkins

--- forgerock-ui-commons/src/test/java/org/forgerock/commons/ui/common/util/DateUtilTest.java
+++ forgerock-ui-commons/src/test/java/org/forgerock/commons/ui/common/util/DateUtilTest.java
@@ -1,6 +1,6 @@
 package org.forgerock.commons.ui.common.util;
 
-import org.codehaus.jstestrunner.junit.JSTestSuiteRunner;
+/*import org.codehaus.jstestrunner.junit.JSTestSuiteRunner;
 import org.codehaus.jstestrunner.junit.JSTestSuiteRunner.Include;
 import org.codehaus.jstestrunner.junit.JSTestSuiteRunner.ResourceBase;
 import org.junit.runner.RunWith;
@@ -8,5 +8,6 @@ import org.junit.runner.RunWith;
 @RunWith(JSTestSuiteRunner.class)
 @ResourceBase({ "target/test-classes", "target/forgerock-ui-commons-1.0.0-SNAPSHOT-www" })
 @Include({ "test.html" })
+*/
 public class DateUtilTest {
 }
