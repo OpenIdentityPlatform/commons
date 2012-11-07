6d4c691c0a1584f9a4de1edf88ea3bb38672cd90
jdabrowski <jdabrowski@localhost>
2012-11-07 14:55:05 +0000

    custom view for user acceptance fix

--- forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/util/CookieHelper.js
+++ forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/util/CookieHelper.js
@@ -22,7 +22,7 @@
  * "Portions Copyrighted [year] [name of copyright owner]"
  */
 
-/*global define*/
+/*global define unescape*/
 
 define("org/forgerock/commons/ui/common/util/CookieHelper", [
 ], function () {
@@ -47,16 +47,16 @@ define("org/forgerock/commons/ui/common/util/CookieHelper", [
     };
     
     obj.getCookie = function(c_name) {
-        var i,x,y,ARRcookies=document.cookie.split(";");
-        for (i=0;i<ARRcookies.length;i++) {
-            x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-            y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-            x=x.replace(/^\s+|\s+$/g,"");
-            if (x==c_name) {
+        var i, x, y, ARRcookies = document.cookie.split(";");
+        for (i=0; i < ARRcookies.length; i++) {
+            x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+            y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+            x = x.replace(/^\s+|\s+$/g,"");
+            if ( x === c_name) {
                 return unescape(y);
             }
         }
-    }
+    };
     
     return obj;
 });
