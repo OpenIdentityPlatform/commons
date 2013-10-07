db18fb27146b260b010d7e74c77a287df5bc4e3d
Matthew Swift <matthew.swift@forgerock.com>
2013-10-07 16:15:25 +0000

    Checkpoint commit for CREST-116: Implement API descriptor resource provider
    * use ResourceName for naming relations
    * add support for multiple API request handlers.

--- json-schema/json-resource/src/main/java/org/forgerock/json/resource/api/Api.java
+++ json-schema/json-resource/src/main/java/org/forgerock/json/resource/api/Api.java
@@ -19,23 +19,19 @@ import static org.forgerock.json.fluent.JsonValue.field;
 import static org.forgerock.json.fluent.JsonValue.object;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 import org.forgerock.json.fluent.JsonValue;
-import org.forgerock.json.resource.ActionRequest;
-import org.forgerock.json.resource.CreateRequest;
-import org.forgerock.json.resource.DeleteRequest;
+import org.forgerock.json.resource.AbstractRequestHandler;
 import org.forgerock.json.resource.NotSupportedException;
-import org.forgerock.json.resource.PatchRequest;
-import org.forgerock.json.resource.QueryRequest;
-import org.forgerock.json.resource.QueryResultHandler;
 import org.forgerock.json.resource.ReadRequest;
 import org.forgerock.json.resource.RequestHandler;
 import org.forgerock.json.resource.Resource;
 import org.forgerock.json.resource.ResultHandler;
 import org.forgerock.json.resource.ServerContext;
-import org.forgerock.json.resource.UpdateRequest;
 
 @SuppressWarnings("javadoc")
 public final class Api {
@@ -43,107 +39,104 @@ public final class Api {
         // Nothing to do.
     }
 
-    public static RequestHandler newApiDescriptorRequestHandler(final ApiDescriptor api) {
-        return new RequestHandler() {
+    private static Object apiToJson(final ApiDescriptor api) {
+        // @formatter:off
+        return object(
+                field("urn", String.valueOf(api.getUrn())),
+                field("name", String.valueOf(api.getUrn().getName())),
+                field("version", String.valueOf(api.getUrn().getVersion())),
+                field("description", api.getDescription()),
+                field("relations", relationsToJson(api.getRelations())),
+                field("resources", resourcesToJson(api.getResources()))
+        );
+        // @formatter:on
+    }
 
-            @Override
-            public void handleUpdate(final ServerContext context, final UpdateRequest request,
-                    final ResultHandler<Resource> handler) {
-                handler.handleError(new NotSupportedException());
-            }
+    private static Object resourcesToJson(final Set<ResourceDescriptor> resources) {
+        final List<Object> json = new ArrayList<Object>(resources.size());
+        for (final ResourceDescriptor resource : resources) {
+            // @formatter:off
+            json.add(object(
+                    field("urn", String.valueOf(resource.getUrn())),
+                    field("name", String.valueOf(resource.getUrn().getName())),
+                    field("version", String.valueOf(resource.getUrn().getVersion())),
+                    field("description", resource.getDescription()),
+                    field("parent", String.valueOf(resource.getParentUrn())),
+                    field("actions", actionsToJson(resource.getActions())),
+                    field("relations", relationsToJson(resource.getRelations()))
+            ));
+            // @formatter:on
+        }
+        return json;
+    }
+
+    private static Object actionsToJson(final Set<ActionDescriptor> actions) {
+        final List<Object> json = new ArrayList<Object>(actions.size());
+        for (final ActionDescriptor action : actions) {
+            // @formatter:off
+            json.add(object(
+                    field("name", action.getName()),
+                    field("description", action.getDescription()),
+                    field("parameters", action.getParameters())
+            ));
+            // @formatter:on
+        }
+        return json;
+    }
+
+    private static Object relationsToJson(final Set<RelationDescriptor> relations) {
+        final List<Object> json = new ArrayList<Object>(relations.size());
+        for (final RelationDescriptor relation : relations) {
+            // @formatter:off
+            json.add(object(
+                    field("name", relation.getResourceName()),
+                    field("description", relation.getDescription()),
+                    field("multiplicity", relation.getMultiplicity()),
+                    field("resource", String.valueOf(relation.getResource().getUrn()))
+            ));
+            // @formatter:on
+        }
+        return json;
+    }
+
+    public static RequestHandler newSingleApiDescriptorRequestHandler(final ApiDescriptor api) {
+        return new AbstractRequestHandler() {
 
             @Override
             public void handleRead(final ServerContext context, final ReadRequest request,
                     final ResultHandler<Resource> handler) {
-                handler.handleError(new NotSupportedException());
-            }
-
-            @Override
-            public void handleQuery(final ServerContext context, final QueryRequest request,
-                    final QueryResultHandler handler) {
-                handler.handleError(new NotSupportedException());
-            }
-
-            @Override
-            public void handlePatch(final ServerContext context, final PatchRequest request,
-                    final ResultHandler<Resource> handler) {
-                handler.handleError(new NotSupportedException());
-            }
-
-            @Override
-            public void handleDelete(final ServerContext context, final DeleteRequest request,
-                    final ResultHandler<Resource> handler) {
-                handler.handleError(new NotSupportedException());
-            }
-
-            @Override
-            public void handleCreate(final ServerContext context, final CreateRequest request,
-                    final ResultHandler<Resource> handler) {
-                handler.handleError(new NotSupportedException());
-            }
-
-            @Override
-            public void handleAction(final ServerContext context, final ActionRequest request,
-                    final ResultHandler<JsonValue> handler) {
-                if (request.getResourceNameObject().isEmpty() && request.getAction().equals("api")) {
-                    // @formatter:off
-                    final JsonValue value = new JsonValue(object(
-                            field("urn", String.valueOf(api.getUrn())),
-                            field("description", api.getDescription()),
-                            field("relations", relationsToJson(api.getRelations())),
-                            field("resources", resourcesToJson(api.getResources()))
-                    ));
-                    // @formatter:on
-                    handler.handleResult(value);
+                if (request.getResourceNameObject().isEmpty()) {
+                    handler.handleResult(new Resource(null, null, new JsonValue(apiToJson(api))));
                 } else {
                     handler.handleError(new NotSupportedException());
                 }
             }
 
-            private Object resourcesToJson(final Set<ResourceDescriptor> resources) {
-                final List<Object> json = new ArrayList<Object>(resources.size());
-                for (final ResourceDescriptor resource : resources) {
-                    // @formatter:off
-                    json.add(object(
-                            field("urn", String.valueOf(resource.getUrn())),
-                            field("description", resource.getDescription()),
-                            field("parent", String.valueOf(resource.getParentUrn())),
-                            field("actions", actionsToJson(resource.getActions())),
-                            field("relations", relationsToJson(resource.getRelations()))
-                    ));
-                    // @formatter:on
-                }
-                return json;
-            }
-
-            private Object actionsToJson(final Set<ActionDescriptor> actions) {
-                final List<Object> json = new ArrayList<Object>(actions.size());
-                for (final ActionDescriptor action : actions) {
-                    // @formatter:off
-                    json.add(object(
-                            field("name", action.getName()),
-                            field("description", action.getDescription()),
-                            field("parameters", action.getParameters())
-                    ));
-                    // @formatter:on
-                }
-                return json;
-            }
-
-            private Object relationsToJson(final Set<RelationDescriptor> relations) {
-                final List<Object> json = new ArrayList<Object>(relations.size());
-                for (final RelationDescriptor relation : relations) {
-                    // @formatter:off
-                    json.add(object(
-                            field("name", relation.getName()),
-                            field("description", relation.getDescription()),
-                            field("multiplicity", relation.getMultiplicity()),
-                            field("resource", String.valueOf(relation.getResource().getUrn()))
-                    ));
-                    // @formatter:on
-                }
-                return json;
-            }
         };
+    }
+
+    public static RequestHandler newMultiApiDescriptorRequestHandler(
+            final Collection<ApiDescriptor> apis) {
+        return new AbstractRequestHandler() {
+
+            @Override
+            public void handleRead(final ServerContext context, final ReadRequest request,
+                    final ResultHandler<Resource> handler) {
+                if (request.getResourceNameObject().isEmpty()) {
+                    final List<Object> values = new ArrayList<Object>(apis.size());
+                    for (final ApiDescriptor api : apis) {
+                        values.add(apiToJson(api));
+                    }
+                    handler.handleResult(new Resource(null, null, new JsonValue(values)));
+                } else {
+                    handler.handleError(new NotSupportedException());
+                }
+            }
+
+        };
+    }
+
+    public static RequestHandler newMultiApiDescriptorRequestHandler(final ApiDescriptor... apis) {
+        return newMultiApiDescriptorRequestHandler(Arrays.asList(apis));
     }
 }
