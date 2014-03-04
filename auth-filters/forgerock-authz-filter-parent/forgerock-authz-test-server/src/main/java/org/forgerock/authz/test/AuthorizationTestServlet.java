f367ee1df8eee7f3435099f5afe2687c6b286eab
Neil Madden <neil.madden@forgerock.com>
2014-03-04 09:30:41 +0000

    CAF-59: CR-3106: Create test module/resource for testing authz context

---
+++ auth-filters/forgerock-authz-filter-parent/forgerock-authz-test-server/src/main/java/org/forgerock/authz/test/AuthorizationTestServlet.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 ForgeRock, AS.
+ *
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ */
+
+package org.forgerock.authz.test;
+
+import org.forgerock.authz.AuthorizationContext;
+import org.forgerock.json.fluent.JsonValue;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * A simple servlet resource that can be used to test authorization filters. The servlet returns any authorization
+ * context attributes that have been set on the session as a JSON object.
+ *
+ * @since 1.4.0
+ */
+@SuppressWarnings("serial") // HttpServlet implements Serializable for unknown reasons
+public class AuthorizationTestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Check if there is any authz context present on the request
+        AuthorizationContext context = AuthorizationContext.forRequest(request);
+        
+        JsonValue json = JsonValue.json(context.getAttributes());
+
+        // Note: JsonValue.toString() may not produce valid JSON, but it should be sufficient for testing.
+        response.setContentType("application/json");
+        response.getWriter().write(json.toString());
+        response.getWriter().close();
+    }
+}
---
+++ auth-filters/forgerock-authz-filter-parent/forgerock-authz-test-server/src/main/java/org/forgerock/authz/test/TestLoggingConfigurator.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014 ForgeRock, AS.
+ *
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ */
+
+package org.forgerock.authz.test;
+
+import org.forgerock.auth.common.AuditLogger;
+import org.forgerock.auth.common.AuditRecord;
+import org.forgerock.authz.AuthorizationLoggingConfigurator;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Configurates the audit logging for the authz test servlet. Ignores everything.
+ *
+ * @since 1.4.0
+ */
+public class TestLoggingConfigurator implements AuthorizationLoggingConfigurator {
+    @Override
+    public AuditLogger<HttpServletRequest> getAuditLogger() {
+        return new AuditLogger<HttpServletRequest>() {
+            @Override
+            public void audit(AuditRecord<HttpServletRequest> auditRecord) {
+                // Ignore
+            }
+        };
+    }
+
+    /**
+     * Static factory method used to get hold of the configurator instance.
+     *
+     * @return a logging configurator instance.
+     */
+    public static AuthorizationLoggingConfigurator getLoggingConfigurator() {
+        return new TestLoggingConfigurator();
+    }
+}
