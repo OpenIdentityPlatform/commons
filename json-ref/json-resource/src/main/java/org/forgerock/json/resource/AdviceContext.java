223c247f793ce3bdee424afd5a27be903e11f170
Phill Cunnington <phill.cunnington@forgerock.com>
2014-11-26 09:43:36 +0000

    CREST-190 CMN-51 Enable AdviceContext to contain multi-value advice

--- json-ref/json-resource/src/main/java/org/forgerock/json/resource/AdviceContext.java
+++ json-ref/json-resource/src/main/java/org/forgerock/json/resource/AdviceContext.java
@@ -16,8 +16,14 @@
 
 package org.forgerock.json.resource;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
 
 import org.forgerock.json.fluent.JsonValue;
 import org.forgerock.util.Reject;
@@ -37,8 +43,15 @@ public class AdviceContext extends AbstractContext {
     /** the persisted attribute name for the advices */
     private static final String ADVICE_ATTR = "advice";
 
+    /** The persisted attribute name for the restricted advice names. */
+    private static final String RESTRICTED_ADVICE_NAMES_ATTR = "restrictedAdviceNames";
+
+    private static final Pattern ALLOWED_RFC_CHARACTERS = Pattern.compile("^[\\x20-\\x7E]*$");
+
+    private final Collection<String> restrictedAdviceNames = new HashSet<String>();
+
     /** advice currently stored for this context is help in this map. **/
-    private final Map<String, String> myAdvice = new HashMap<String, String>();
+    private final Map<String, List<String>> advice = new TreeMap<String, List<String>>(String.CASE_INSENSITIVE_ORDER);
 
     /**
      * Creates a new AdviceContext with the provided parent
@@ -46,9 +59,11 @@ public class AdviceContext extends AbstractContext {
      * @param parent
      *            The parent context.
      */
-    public AdviceContext(Context parent) {
+    public AdviceContext(Context parent, Collection<String> restrictedAdviceNames) {
         super(parent);
-        data.put(ADVICE_ATTR, myAdvice);
+        this.restrictedAdviceNames.addAll(restrictedAdviceNames);
+        data.put(RESTRICTED_ADVICE_NAMES_ATTR, restrictedAdviceNames);
+        data.put(ADVICE_ATTR, advice);
     }
 
     /**
@@ -65,7 +80,8 @@ public class AdviceContext extends AbstractContext {
      */
     AdviceContext(final JsonValue savedContext, final PersistenceConfig config) throws ResourceException {
         super(savedContext, config);
-        myAdvice.putAll(data.get(ADVICE_ATTR).asMap(String.class));
+        restrictedAdviceNames.addAll(data.get(RESTRICTED_ADVICE_NAMES_ATTR).asSet(String.class));
+        advice.putAll(data.get(ADVICE_ATTR).asMapOfList(String.class));
     }
 
     /**
@@ -73,23 +89,40 @@ public class AdviceContext extends AbstractContext {
      * 
      * @return the advices contained within this context.
      */
-    public Map<String, String> getAdvices() {
-        return myAdvice;
-    }
-
-    /**
-     * Adds a piece of advice to the context, which can be retrieved and later returned to the user.
-     *
-     * @param adviceName Name of the advice to return to the user. Not null.
-     * @param advice Human-readable advice to return to the user. Not null.
-     */
-    public void putAdvice(String adviceName, String advice) {
-        Reject.ifNull(adviceName, advice);
-        myAdvice.put(adviceName, advice);
+    public Map<String, List<String>> getAdvices() {
+        return advice;
     }
 
     @Override
     public String getContextName() {
         return CONTEXT_NAME;
+    }
+
+    /**
+     * Adds advice to the context, which can be retrieved and later returned to the user.
+     *
+     * @param adviceName Name of the advice to return to the user. Not null.
+     * @param advices Human-readable advice to return to the user. Not null.
+     */
+    public void putAdvice(String adviceName, String... advices) {
+        Reject.ifNull(adviceName, advices);
+        Reject.ifTrue(isRestrictedAdvice(adviceName), "Illegal use of restricted advice name, " + adviceName);
+        for (String adviceEntry : advices) {
+            Reject.ifTrue(!isRfcCompliant(adviceEntry), "Advice contains illegal characters in, " + adviceEntry);
+        }
+        List<String> adviceEntry = advice.get(adviceName);
+        if (adviceEntry == null) {
+            adviceEntry = new ArrayList<String>();
+            advice.put(adviceName, adviceEntry);
+        }
+        adviceEntry.addAll(Arrays.asList(advices));
+    }
+
+    private boolean isRfcCompliant(String advice) {
+        return ALLOWED_RFC_CHARACTERS.matcher(advice).matches();
+    }
+
+    private boolean isRestrictedAdvice(String adviceName) {
+        return restrictedAdviceNames.contains(adviceName);
     }
 }
