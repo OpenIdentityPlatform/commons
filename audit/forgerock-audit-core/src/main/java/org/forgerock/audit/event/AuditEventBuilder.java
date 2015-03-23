33f541199855b34b37b6a0765e5a7da544771998
Nicolas Capponi <nicolas.capponi@forgerock.com>
2015-03-23 09:45:04 +0000

    CAUD-19 CMN-87 API for common audit events building
    Support for creating audit events for "audit/access" topic

---
+++ audit/forgerock-audit-core/src/main/java/org/forgerock/audit/event/AuditEventBuilder.java
@@ -0,0 +1,317 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Copyright 2015 ForgeRock AS
+ */
+package org.forgerock.audit.event;
+
+import static org.forgerock.json.fluent.JsonValue.*;
+
+import java.util.Arrays;
+
+import org.forgerock.audit.event.AuditEventBuilderTest.OpenProductAccessAuditEventBuilder;
+import org.forgerock.json.fluent.JsonValue;
+
+/**
+ * Root builder for all audit events.
+ *
+ * @param <T> the type of the builder
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+public abstract class AuditEventBuilder<T extends AuditEventBuilder<T>> {
+
+    /** Represents the event as a JSON value. */
+    protected final JsonValue jsonValue = json(object());
+
+    /** A reference to this object with its actual type. */
+    //private T self;
+
+    /**
+     * Creates the builder.
+     */
+    protected AuditEventBuilder() {
+        //self = self();
+    }
+
+    /**
+     * Returns this object, as its actual type.
+     *
+     * @return this object
+     */
+    protected abstract T self();
+
+    /**
+     * Generates the audit event.
+     *
+     * @return the audit event
+     */
+    public AuditEvent toEvent() {
+        return new AuditEvent(jsonValue);
+    }
+
+    /**
+     * Sets the provided time stamp for the event.
+     *
+     * @param t the time stamp.
+     * @return this builder
+     */
+    public T timestamp(String t) {
+        jsonValue.put("timestamp", t);
+        return self();
+    }
+
+    /**
+     * Sets the provided transactionId for the event.
+     *
+     * @param id the transaction id.
+     * @return this builder
+     */
+    public T transactionId(String id) {
+        jsonValue.put("transactionId", id);
+        return self();
+    }
+
+    /**
+     * Sets a field that is not predefined in the builder.
+     *
+     * @param name the name of the field.
+     * @param value the value of the field
+     * @return this builder
+     */
+    public T aField(String name, Object value) {
+        jsonValue.put(name, value);
+        return self();
+    }
+
+    /**
+     * Builder for audit access events.
+     * <p>
+     * This builder should not be used directly but be specialized for each product to allow to define
+     * new specific fields, e.g
+     * <pre>
+     * <code>
+     * class MyProductAccessAuditEventBuilder{@code <T extends MyProductAccessAuditEventBuilder<T>>}
+        extends AccessAuditEventBuilder{@code <T>} {
+     *
+     *    public static {@code <T>} OpenProductAccessAuditEventBuilder{@code <?>} productAccessEvent() {
+     *       return new OpenProductAccessAuditEventBuilder();
+     *    }
+     *
+     *    public T someField(String v) {
+     *      jsonValue.put("someField", v);
+     *      return self();
+     *    }
+     *
+     *    {@literal @}Override
+     *    protected T self() {
+     *      return (T) this;
+     *    }
+     *
+     *    ...
+     * }
+     * </code>
+     * </pre>
+     */
+    static class AccessAuditEventBuilder<T extends AccessAuditEventBuilder<T>> extends AuditEventBuilder<T> {
+
+        protected AccessAuditEventBuilder() {
+            // reduced visibility
+            super();
+        }
+
+        /**
+         * Starts to build an audit access event.
+         * <p>
+         * Note: it is preferable to use a specialized builder that allow to add
+         * fields specific to a product.
+         *
+         * @return an audit access event builder
+         */
+        public static AccessAuditEventBuilder<?> accessEvent() {
+            return new AccessAuditEventBuilder();
+        }
+
+        /**
+         * Sets the provided message id for the event.
+         *
+         * @param id the message id.
+         * @return this builder
+         */
+        public T messageId(String id) {
+            jsonValue.put("messageId", id);
+            return self();
+        }
+
+        /**
+         * Sets the provided authentication id for the event.
+         *
+         * @param id the authentication id.
+         * @return this builder
+         */
+        public T authenticationId(String id) {
+            jsonValue.put("authenticationId", id);
+            return self();
+        }
+
+        /**
+         * Sets the provided server values for the event.
+         *
+         * @param ip the ip of the server.
+         * @param port the port of the server.
+         * @return this builder
+         */
+        public T server(String ip, String port) {
+            JsonValue object = json(object(
+                    field("ip", ip),
+                    field("port", port)));
+            jsonValue.put("server", object);
+            return self();
+        }
+
+        /**
+         * Sets the provided client ip and port for the event.
+         *
+         * @param ip the ip of the client.
+         * @param port the port of the client.
+         * @return this builder
+         */
+        public T client(String ip, String port) {
+            JsonValue object = json(object(
+                    field("ip", ip),
+                    field("port", port)));
+            jsonValue.put("client", object);
+            return self();
+        }
+
+        /**
+         * Sets the provided client hostname, ip and port for the event.
+         *
+         * @param host the hostname of the client.
+         * @param ip the ip of the client.
+         * @param port the port of the client.
+         * @return this builder
+         */
+        public T client(String host, String ip, String port) {
+            JsonValue object = json(object(
+                    field("host", host),
+                    field("ip", ip),
+                    field("port", port)));
+            jsonValue.put("client", object);
+            return self();
+        }
+
+        /**
+         * Sets the provided resource operation method and action for the event.
+         *
+         * @param method the method of the operation (for CREST, expect one method in CRUDPAQ).
+         * @param action the detailed action of the operation.
+         * @return this builder
+         */
+        public T resourceOperation(String method, String action) {
+            JsonValue object = json(object(
+                    field("method", method),
+                    field("action", action)));
+            jsonValue.put("resourceOperation", object);
+            return self();
+        }
+
+        /**
+         * Sets the provided resource operation method for the event.
+         *
+         * If the method is an Action, use {@code resourceOperation(String, String)} method instead.
+         *
+         * @param method the method of the operation (for CREST, expect one method in CRUDPQ).
+         * @return this builder
+         */
+        public T resourceOperation(String method) {
+            JsonValue object = json(object(field("method", method)));
+            jsonValue.put("resourceOperation", object);
+            return self();
+        }
+
+        /**
+         * Sets the provided authorizationId fields for the event.
+         *
+         * @param component the component part of the authorization id.
+         * @param id the id part of the authorization id.
+         * @param roles the list of roles. Roles are optional.
+         * @return this builder
+         */
+        public T authorizationId(String component, String id, String...roles) {
+            JsonValue object = json(object(
+                    field("component", component),
+                    field("id", id)));
+            if (roles != null && roles.length > 0) {
+                Object roleList = json(array(Arrays.copyOf(roles, roles.length, Object[].class)));
+                object.put("roles", roleList);
+            }
+            jsonValue.put("authorizationId", object);
+            return self();
+        }
+
+        /**
+         * Sets the provided HTTP fields for the event.
+         *
+         * @param method the HTTP method.
+         * @param path the path of HTTP request.
+         * @param queryString the query string of HTTP request.
+         * @param headers the list of headers of HTTP request. The headers are optional.
+         * @return this builder
+         */
+        public T http(String method, String path, String queryString, String...headers) {
+            JsonValue object = json(object(
+                    field("method", method),
+                    field("path", path),
+                    field("queryString", queryString)));
+            if (headers != null && headers.length > 0) {
+                Object headersList = json(array(Arrays.copyOf(headers, headers.length, Object[].class)));
+                object.put("headers", headersList);
+            }
+            jsonValue.put("http", object);
+            return self();
+        }
+
+        /**
+         * Sets the provided response for the event.
+         *
+         * @param status the status of the operation.
+         * @param elapsedTime the execution time of the action.
+         * @return this builder
+         */
+        public T response(String status, String elapsedTime) {
+            JsonValue object = json(object(
+                    field("status", status),
+                    field("elapsedTime", elapsedTime)));
+            jsonValue.put("response", object);
+            return self();
+        }
+
+        @Override
+        protected T self() {
+            return (T) this;
+        }
+
+    }
+
+}
---
+++ audit/forgerock-audit-core/src/test/java/org/forgerock/audit/event/AuditEventBuilderTest.java
@@ -0,0 +1,125 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2015 ForgeRock AS.
+ */
+package org.forgerock.audit.event;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.forgerock.audit.event.AuditEventBuilderTest.OpenProductAccessAuditEventBuilder.productAccessEvent;
+
+import org.forgerock.audit.event.AuditEventBuilder.AccessAuditEventBuilder;
+import org.forgerock.json.fluent.JsonValue;
+import org.testng.annotations.Test;
+
+@SuppressWarnings("javadoc")
+public class AuditEventBuilderTest {
+
+    /**
+     * Example builder of audit access events for some imaginary product "OpenProduct".
+     */
+    @SuppressWarnings("rawtypes")
+    static class OpenProductAccessAuditEventBuilder<T extends OpenProductAccessAuditEventBuilder<T>>
+        extends AccessAuditEventBuilder<T> {
+
+        private OpenProductAccessAuditEventBuilder() {
+            super();
+        }
+
+        public static <T> OpenProductAccessAuditEventBuilder<?> productAccessEvent() {
+            return new OpenProductAccessAuditEventBuilder();
+        }
+
+        public T openField(String v) {
+            jsonValue.put("open", v);
+            return self();
+        }
+
+        @Override
+        protected T self() {
+            return (T) this;
+        }
+    }
+
+    @Test
+    public void ensureEventIsCorrectlyBuilt() {
+        AuditEvent event = productAccessEvent()
+                .transactionId("transactionId")
+                .messageId("IDM-sync-10")
+                .client("cip", "cport")
+                .server("sip", "sport")
+                .authorizationId("managed/user", "aegloff", "openidm-admin", "openidm-authorized")
+                .authenticationId("someone@forgerock.com")
+                .resourceOperation("action", "reconcile")
+                .http("GET", "/some/path", "p1=v1&p2=v2", "Content-Length: 200", "Content-Type: application/json")
+                .response("200", "12")
+                .openField("value")
+                .aField("field", "fieldValue")
+                .toEvent();
+
+        JsonValue value = event.getValue();
+        assertThat(value.get("transactionId").getObject()).isEqualTo("transactionId");
+        assertThat(value.get("messageId").getObject()).isEqualTo("IDM-sync-10");
+        assertThat(value.get("server").get("ip").getObject()).isEqualTo("sip");
+        assertThat(value.get("server").get("port").getObject()).isEqualTo("sport");
+        assertThat(value.get("http").get("method").getObject()).isEqualTo("GET");
+        assertThat(value.get("authorizationId").get("id").getObject()).isEqualTo("aegloff");
+        assertThat(value.get("resourceOperation").get("method").getObject()).isEqualTo("action");
+        assertThat(value.get("response").get("status").getObject()).isEqualTo("200");
+        assertThat(value.get("open").getObject()).isEqualTo("value");
+        assertThat(value.get("field").getObject()).isEqualTo("fieldValue");
+    }
+
+
+    @Test
+    public void ensureBuilderMethodsCanBeCalledInAnyOrder() {
+        AuditEvent event1 = productAccessEvent()
+                .server("ip", "port")
+                .client("cip", "cport")
+                .openField("value")
+                .transactionId("transactionId").toEvent();
+        assertEvent(event1);
+
+        AuditEvent event2 = productAccessEvent()
+                .client("cip", "cport")
+                .openField("value")
+                .server("ip", "port")
+                .transactionId("transactionId").toEvent();
+        assertEvent(event2);
+
+        AuditEvent event3 = productAccessEvent()
+                .openField("value")
+                .transactionId("transactionId")
+                .client("cip", "cport")
+                .server("ip", "port").toEvent();
+        assertEvent(event3);
+
+        AuditEvent event4 = productAccessEvent()
+                .transactionId("transactionId")
+                .client("cip", "cport")
+                .openField("value")
+                .server("ip", "port").toEvent();
+        assertEvent(event4);
+
+    }
+
+    private void assertEvent(AuditEvent event) {
+        JsonValue value = event.getValue();
+        assertThat(value.get("open").getObject()).isEqualTo("value");
+        assertThat(value.get("server").get("ip").getObject()).isEqualTo("ip");
+        assertThat(value.get("server").get("port").getObject()).isEqualTo("port");
+        assertThat(value.get("client").get("ip").getObject()).isEqualTo("cip");
+        assertThat(value.get("client").get("port").getObject()).isEqualTo("cport");
+        assertThat(value.get("transactionId").getObject()).isEqualTo("transactionId");
+    }
+}
