95e2872f2fb1efd56bb93ec242b07407117e4917
Laurent Vaills <laurent.vaills@forgerock.com>
2015-04-30 22:04:08 +0000

    Add status UNAUTHORIZED (401).

--- http-framework/http-core/src/main/java/org/forgerock/http/protocol/Status.java
+++ http-framework/http-core/src/main/java/org/forgerock/http/protocol/Status.java
@@ -708,6 +708,21 @@ public final class Status {
     public static final Status BAD_REQUEST = newStatus(400, "Bad Request");
 
     /**
+     * The request requires user authentication. The response MUST include a WWW-Authenticate header field (section
+     * 14.47) containing a challenge applicable to the requested resource. The client MAY repeat the request with a
+     * suitable Authorization header field (section 14.8). If the request already included Authorization credentials,
+     * then the 401 response indicates that authorization has been refused for those credentials. If the 401 response
+     * contains the same challenge as the prior response, and the user agent has already attempted authentication at
+     * least once, then the user SHOULD be presented the entity that was given in the response, since that entity might
+     * include relevant diagnostic information. HTTP access authentication is explained in
+     * "HTTP Authentication: Basic and Digest Access Authentication" (see RFC 2617).
+     *
+     * @see http://www.ietf.org/rfc/rfc2616.txt
+     * @see http://www.ietf.org/rfc/rfc2617.txt
+     */
+    public static final Status UNAUTHORIZED = newStatus(401, "Unauthorized");
+
+    /**
      * The 402 (Payment Required) status code is reserved for future use.
      */
     public static final Status PAYMENT_REQUIRED = newStatus(402, "Payment Required");
