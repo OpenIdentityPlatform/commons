e9c8c5780b78a1bb1bc6bb863822dabf197b6bb2
Chad Kienle <chad.kienle@forgerock.com>
2015-06-05 17:05:26 +0000

    [CREST-243][CMN-156] Improved Javadoc for QueryFilter and associated classes.

--- rest/json-resource/src/main/java/org/forgerock/json/resource/QueryFilters.java
+++ rest/json-resource/src/main/java/org/forgerock/json/resource/QueryFilters.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyrighted [year] [name of copyright owner]".
  *
- * Copyright © 2012 ForgeRock AS. All rights reserved.
+ * Copyright 2012-2015 ForgeRock AS. All rights reserved.
  */
 
 package org.forgerock.json.resource;
@@ -26,36 +26,8 @@ import org.forgerock.util.query.QueryFilterVisitor;
 /**
  * Convenience methods to create {@link org.forgerock.util.query.QueryFilter} that
  * specify fields in terms of {@link org.forgerock.json.fluent.JsonPointer} instances.
- * <p>
- * A query string has the following string representation:
  *
- * <pre>
- * Expr           = OrExpr
- * OrExpr         = AndExpr ( 'or' AndExpr ) *
- * AndExpr        = NotExpr ( 'and' NotExpr ) *
- * NotExpr        = '!' PrimaryExpr | PrimaryExpr
- * PrimaryExpr    = '(' Expr ')' | ComparisonExpr | PresenceExpr | LiteralExpr
- * ComparisonExpr = Pointer OpName JsonValue
- * PresenceExpr   = Pointer 'pr'
- * LiteralExpr    = 'true' | 'false'
- * Pointer        = JSON pointer
- * OpName         = 'eq' |  # equal to
- *                  'co' |  # contains
- *                  'sw' |  # starts with
- *                  'lt' |  # less than
- *                  'le' |  # less than or equal to
- *                  'gt' |  # greater than
- *                  'ge' |  # greater than or equal to
- *                  STRING  # extended operator
- * JsonValue      = NUMBER | BOOLEAN | '"' UTF8STRING '"'
- * STRING         = ASCII string not containing white-space
- * UTF8STRING     = UTF-8 string possibly containing white-space
- * </pre>
- *
- * Note that white space, parentheses, and exclamation characters need URL
- * encoding in HTTP query strings.
- *
- * @see org.forgerock.util.query.QueryFilter
+ * @see org.forgerock.util.query.QueryFilterParser
  */
 public final class QueryFilters {
 
