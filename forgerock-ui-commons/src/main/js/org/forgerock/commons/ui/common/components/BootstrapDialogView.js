027f749979aa8d0620d29bcc3547037e11d1aa62
Jake Feasel <jake.feasel@forgerock.com>
2015-07-19 13:30:21 -0700

    Fixes bug with BootstrapDialogView loading content

--- forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/components/BootstrapDialogView.js
+++ forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/components/BootstrapDialogView.js
@@ -37,7 +37,7 @@ define("org/forgerock/commons/ui/common/components/BootstrapDialogView", [
         show: function(callback) {
             var self = this;
             self.setButtons();
-            $.when(ModuleLoader.load("bootstrap-dialog"), self.loadContent).then(_.bind(
+            $.when(ModuleLoader.load("bootstrap-dialog"), self.loadContent()).then(_.bind(
                 function (BootstrapDialog, content) {
                     self.type = self.type || BootstrapDialog.TYPE_DEFAULT;
                     self.size = self.size || BootstrapDialog.SIZE_NORMAL;
