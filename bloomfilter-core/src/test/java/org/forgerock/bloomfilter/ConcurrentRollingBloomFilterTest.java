835d1cef3da3e1551e899455db76050e6019529f
Phill Cunnington <phill.cunnington@forgerock.com>
2015-10-21 17:58:15 +0100

    Update to use forgerock-bom

--- bloomfilter-core/src/test/java/org/forgerock/bloomfilter/ConcurrentRollingBloomFilterTest.java
+++ bloomfilter-core/src/test/java/org/forgerock/bloomfilter/ConcurrentRollingBloomFilterTest.java
@@ -16,8 +16,8 @@
 
 package org.forgerock.bloomfilter;
 
-import junit.framework.Assert;
 import org.forgerock.guava.common.hash.Funnels;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
