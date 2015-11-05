ba8370051f6dec50b00e5722a4a1965c9cb35c8c
Jason Lemay <jason.lemay@forgerock.com>
2015-11-05 15:22:01 -0800

    CAUD-189 Update the JDBCAuditEventHandler repo and sql files

--- audit/forgerock-audit-handler-jdbc/src/main/resources/db/oracle/audit.sql
+++ audit/forgerock-audit-handler-jdbc/src/main/resources/db/oracle/audit.sql
@@ -3,29 +3,31 @@
 -- -----------------------------------------------------
 PROMPT Creating Table auditaccess ...
 CREATE TABLE auditaccess (
-  id VARCHAR2(38 CHAR) NOT NULL,
-  timestamp VARCHAR2(29 CHAR) NOT NULL,
-  transactionid VARCHAR2(56 CHAR) NOT NULL,
+  id VARCHAR2(56 CHAR) NOT NULL,
+  timestamp_ VARCHAR2(29 CHAR) NOT NULL,
+  transactionid VARCHAR2(255 CHAR) NOT NULL,
   eventname VARCHAR2(255 CHAR),
-  authentication_id VARCHAR2(255 CHAR),
+  userid VARCHAR2(255 CHAR),
+  trackingids CLOB,
   server_ip VARCHAR2(40 CHAR),
   server_port VARCHAR2(5 CHAR),
   client_host VARCHAR2(255 CHAR),
   client_ip VARCHAR2(40 CHAR),
   client_port VARCHAR2(5 CHAR),
-  authorizationid_id CLOB,
-  authorizationid_roles VARCHAR2(1024 CHAR),
-  authorizationid_component VARCHAR2(255 CHAR),
-  resource_uri VARCHAR2(255 CHAR),
-  resource_protocol VARCHAR2(10 CHAR),
-  resource_method VARCHAR2(10 CHAR),
-  resource_detail VARCHAR2(255 CHAR),
-  http_method VARCHAR2(10 CHAR),
-  http_path VARCHAR2(255 CHAR),
-  http_querystring VARCHAR2(255 CHAR),
-  http_headers CLOB,
-  status VARCHAR2(20 CHAR),
-  elapsedtime VARCHAR2(13 CHAR)
+  request_protocol VARCHAR2(255 CHAR) NULL ,
+  request_operation VARCHAR2(255 CHAR) NULL ,
+  request_detail CLOB NULL ,
+  http_request_secure VARCHAR2(255 CHAR) NULL ,
+  http_request_method VARCHAR2(7 CHAR) NULL ,
+  http_request_path VARCHAR2(255 CHAR) NULL ,
+  http_request_queryparameters CLOB(2M) NULL ,
+  http_request_headers CLOB NULL ,
+  http_request_cookies CLOB NULL ,
+  http_response_headers CLOB NULL ,
+  response_status VARCHAR2(10 CHAR) NULL ,
+  response_statuscode VARCHAR2(255 CHAR) NULL ,
+  response_elapsedtime VARCHAR2(255 CHAR) NULL ,
+  response_elapsedtimeunits VARCHAR2(255 CHAR) NULL
 );
 
 
@@ -46,16 +48,16 @@ ENABLE
 -- -----------------------------------------------------
 PROMPT Creating TABLE auditauthentication ...
 CREATE TABLE auditauthentication (
-  id VARCHAR2(38 CHAR) NOT NULL,
-  transactionid VARCHAR2(56 CHAR) NOT NULL,
-  timestamp VARCHAR2(29 CHAR) NOT NULL,
-  authentication_id VARCHAR2(255 CHAR),
+  id VARCHAR2(56 CHAR) NOT NULL,
+  transactionid VARCHAR2(255 CHAR) NOT NULL,
+  timestamp_ VARCHAR2(29 CHAR) NOT NULL,
+  userid VARCHAR2(255 CHAR),
   eventname VARCHAR2(50 CHAR),
   result VARCHAR2(255 CHAR),
   principals CLOB,
   context CLOB,
-  sessionid VARCHAR2(255 CHAR),
-  entries CLOB
+  entries CLOB,
+  trackingids CLOB
 );
 
 COMMENT ON COLUMN auditauthentication.timestamp IS 'Date format: 2011-09-09T14:58:17.654+02:00'
@@ -76,18 +78,17 @@ ENABLE
 -- -----------------------------------------------------
 PROMPT Creating Table auditactivity ...
 CREATE TABLE auditactivity (
-  id VARCHAR2(38 CHAR) NOT NULL,
-  timestamp VARCHAR2(29 CHAR) NOT NULL,
-  transactionid VARCHAR2(56 CHAR) NOT NULL,
+  id VARCHAR2(56 CHAR) NOT NULL,
+  timestamp_ VARCHAR2(29 CHAR) NOT NULL,
+  transactionid VARCHAR2(255 CHAR) NOT NULL,
   eventname VARCHAR2(255 CHAR),
-  authentication_id VARCHAR2(255 CHAR),
+  userid VARCHAR2(255 CHAR),
+  trackingids CLOB,
   runas VARCHAR2(255 CHAR),
-  resource_uri VARCHAR2(255 CHAR),
-  resource_protocol VARCHAR2(10 CHAR),
-  resource_method VARCHAR2(10 CHAR),
-  resource_detail VARCHAR2(255 CHAR),
-  before CLOB,
-  after CLOB,
+  objectid VARCHAR2(255 CHAR) NULL ,
+  operation VARCHAR2(255 CHAR) NULL ,
+  beforeObject CLOB,
+  afterObject CLOB,
   changedfields VARCHAR2(255 CHAR),
   rev VARCHAR2(255 CHAR)
 );
@@ -116,18 +117,17 @@ CREATE INDEX idx_activity_txid ON auditactivity
 -- -----------------------------------------------------
 PROMPT Creating Table auditconfig ...
 CREATE TABLE auditconfig (
-  id VARCHAR2(38 CHAR) NOT NULL,
-  timestamp VARCHAR2(29 CHAR) NOT NULL,
-  transactionid VARCHAR2(56 CHAR) NOT NULL,
+  id VARCHAR2(56 CHAR) NOT NULL,
+  timestamp_ VARCHAR2(29 CHAR) NOT NULL,
+  transactionid VARCHAR2(255 CHAR) NOT NULL,
   eventname VARCHAR2(255 CHAR),
-  authentication_id VARCHAR2(255 CHAR),
+  userid VARCHAR2(255 CHAR),
+  trackingids CLOB,
   runas VARCHAR2(255 CHAR),
-  resource_uri VARCHAR2(255 CHAR),
-  resource_protocol VARCHAR2(10 CHAR),
-  resource_method VARCHAR2(10 CHAR),
-  resource_detail VARCHAR2(255 CHAR),
-  before CLOB,
-  after CLOB,
+  objectid VARCHAR2(255 CHAR) NULL ,
+  operation VARCHAR2(255 CHAR) NULL ,
+  beforeObject CLOB,
+  afterObject CLOB,
   changedfields VARCHAR2(255 CHAR),
   rev VARCHAR2(255 CHAR)
 );
