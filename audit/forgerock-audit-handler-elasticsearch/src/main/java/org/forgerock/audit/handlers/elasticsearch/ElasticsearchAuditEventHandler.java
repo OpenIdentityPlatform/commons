82f12f8ff571cd481ef2066eefd42a0d98033bcf
Travis Haagen <travis.haagen@forgerock.com>
2016-02-01 14:39:06 -0800

    CAUD-303 Elasticsearch read operation

--- audit/forgerock-audit-handler-elasticsearch/src/main/java/org/forgerock/audit/handlers/elasticsearch/ElasticsearchAuditEventHandler.java
+++ audit/forgerock-audit-handler-elasticsearch/src/main/java/org/forgerock/audit/handlers/elasticsearch/ElasticsearchAuditEventHandler.java
@@ -18,8 +18,10 @@ package org.forgerock.audit.handlers.elasticsearch;
 import static org.forgerock.json.resource.Responses.newQueryResponse;
 import static org.forgerock.json.resource.Responses.newResourceResponse;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.forgerock.audit.Audit;
 import org.forgerock.audit.events.EventTopicsMetaData;
 import org.forgerock.audit.events.handlers.AuditEventHandler;
@@ -28,9 +30,10 @@ import org.forgerock.http.Client;
 import org.forgerock.http.header.ContentTypeHeader;
 import org.forgerock.http.protocol.Request;
 import org.forgerock.http.protocol.Response;
-import org.forgerock.http.protocol.Status;
+import org.forgerock.json.JsonException;
 import org.forgerock.json.JsonValue;
 import org.forgerock.json.resource.InternalServerErrorException;
+import org.forgerock.json.resource.NotFoundException;
 import org.forgerock.json.resource.QueryRequest;
 import org.forgerock.json.resource.QueryResourceHandler;
 import org.forgerock.json.resource.QueryResponse;
@@ -50,6 +53,7 @@ import static org.forgerock.audit.handlers.elasticsearch.ElasticsearchAuditEvent
 public class ElasticsearchAuditEventHandler extends AuditEventHandlerBase {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticsearchAuditEventHandler.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     private final ElasticsearchAuditEventHandlerConfiguration configuration;
     private final Client client;
@@ -91,34 +95,52 @@ public class ElasticsearchAuditEventHandler extends AuditEventHandlerBase {
     @Override
     public Promise<ResourceResponse, ResourceException> readEvent(final Context context, final String topic,
             final String resourceId) {
-        // TODO
-        return newResourceResponse(resourceId, null, null).asPromise();
+        try {
+            final Request request = new Request();
+            request.setMethod("GET");
+            request.setUri(buildEventUri(topic, resourceId));
+
+            final Response response = client.send(request).get();
+            if (!response.getStatus().isSuccessful()) {
+                return getResourceExceptionPromise(topic, resourceId, response);
+            }
+
+            // the original audit JSON is under _source, and we also add back the _id
+            JsonValue jsonValue = toJsonValue(response.getEntity().toString());
+            jsonValue = jsonValue.get("_source");
+            jsonValue.put("_id", resourceId);
+            return newResourceResponse(resourceId, null, jsonValue).asPromise();
+        } catch (Exception e) {
+            final String error = String.format("Unable to read audit entry for topic=%s, _id=%s", topic, resourceId);
+            LOGGER.error(error, e);
+            return new InternalServerErrorException(error, e).asPromise();
+        }
     }
 
     @Override
     public Promise<ResourceResponse, ResourceException> publishEvent(final Context context, final String topic,
             final JsonValue event) {
+        String resourceId = null;
         try {
             // _id is a protected Elasticsearch field, so read it and remove it
-            final String eventId = event.get("_id").asString();
+            resourceId = event.get("_id").asString();
             event.remove("_id");
             final String jsonPayload = ElasticsearchUtil.normalizeJson(event);
 
             final Request request = new Request();
             request.setMethod("PUT");
-            request.setUri(buildEventUri(topic, eventId));
+            request.setUri(buildEventUri(topic, resourceId));
             if (jsonPayload != null) {
                 request.getHeaders().put(ContentTypeHeader.NAME, "application/json; charset=UTF-8");
                 request.getEntity().setBytes(jsonPayload.getBytes(StandardCharsets.UTF_8));
             }
-            final Response response = client.send(request).get();
 
-            final Status status = response.getStatus();
-            if (!status.isSuccessful()) {
-                LOGGER.warn("Request failed: " + status);
+            final Response response = client.send(request).get();
+            if (!response.getStatus().isSuccessful()) {
+                return getResourceExceptionPromise(topic, resourceId, response);
             }
         } catch (Exception e) {
-            final String error = String.format("Unable to create audit entry for %s", topic);
+            final String error = String.format("Unable to create audit entry for topic=%s, _id=%s", topic, resourceId);
             LOGGER.error(error, e);
             return new InternalServerErrorException(error, e).asPromise();
         }
@@ -137,5 +159,36 @@ public class ElasticsearchAuditEventHandler extends AuditEventHandlerBase {
         final ConnectionConfiguration connection = configuration.getConnection();
         return (connection.isUseSSL() ? "https" : "http") + "://" + connection.getHost() + "/" +
                 indexMapping.getIndexName() + "/" + topic + "/" + eventId;
+    }
+
+    /**
+     * Gets an {@code Exception} {@link Promise} containing an Elasticsearch HTTP response status and payload.
+     *
+     * @param response HTTP response
+     * @return {@code Exception} {@link Promise}
+     */
+    protected static Promise<ResourceResponse, ResourceException> getResourceExceptionPromise(
+            final String topic, final String resourceId, final Response response) {
+        if (response.getStatus().getCode() == ResourceException.NOT_FOUND) {
+            return new NotFoundException("Object " + resourceId + " not found in audit/" + topic).asPromise();
+        }
+        final String message = "Elasticsearch response (audit/" + topic + "/" + resourceId + "): "
+                + response.getEntity();
+        return ResourceException.newResourceException(response.getStatus().getCode(), message).asPromise();
+    }
+
+    /**
+     * Parses a JSON string into a {@link JsonValue}.
+     *
+     * @param json JSON string
+     * @return {@link JsonValue}
+     */
+    protected static JsonValue toJsonValue(final String json) {
+        try {
+            final Object parsedValue = OBJECT_MAPPER.readValue(json, Object.class);
+            return new JsonValue(parsedValue);
+        } catch (IOException ex) {
+            throw new JsonException("String is not valid JSON", ex);
+        }
     }
 }
