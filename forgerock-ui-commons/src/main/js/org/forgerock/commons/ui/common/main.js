00227c39968b35045fca59ae5fe229fa071955d6
Joe Bandenburg <joe.bandenburg@forgerock.com>
2016-04-27 10:55:30 -0700

    Update copyright headers

--- forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/main.js
+++ forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/main.js
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2011-2015 ForgeRock AS.
+ * Copyright 2011-2016 ForgeRock AS.
  */
 
 // this module is merely a useful construct for identifying
