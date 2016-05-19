74df78e1e5892a1171cc07bad416266adff742df
Jake Feasel <jake.feasel@forgerock.com>
2016-05-19 10:35:39 -0700

    CUI-140 - Change patch syntax for arrays so they are treated as sets, addressed by value rather than position

--- forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/util/ObjectUtil.js
+++ forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/util/ObjectUtil.js
@@ -39,7 +39,9 @@ define([
                     return p[1] !== undefined;
                 })
                 .map(function (p) {
-                    if (_.indexOf(["string","boolean","number"], (typeof p[1])) !== -1 || _.isEmpty(p[1])) {
+                    if (_.indexOf(["string","boolean","number"], (typeof p[1])) !== -1 ||
+                        _.isEmpty(p[1]) ||
+                        _.isArray(p[1])) {
                         return { "pointer": "/" + p[0], "value": p[1]};
                     } else {
                         return _.map(pointerList(p[1]), function (child) {
@@ -125,6 +127,54 @@ define([
     };
 
     /**
+     * Compare to Array values, interpeted as sets, to see if they contain the same values.
+     * Important distinctive behavior of sets - order doesn't matter for equality.
+     * @param {Array} set1 - an set of any values. Nested Arrays will also be interpreted as sets.
+     * @param {Array} set2 - an set of any values. Nested Arrays will also be interpreted as sets.
+     * Examples:
+     *  isEqualSet([1], [1]) -> true
+     *  isEqualSet([1], [1,3]) -> false
+     *  isEqualSet([3,1], [1,3]) -> true
+     *  isEqualSet([3,{a:1},1], [1,3,{a:1}]) -> true
+     *  isEqualSet([3,{a:1},1], [1,3,{a:2}]) -> false
+     *  isEqualSet([3,{a:1},['b','a'],1], [1,3,{a:1},['a','b']]) -> true
+     */
+    obj.isEqualSet = function (set1, set2) {
+        var traverseSet = function (targetSet, result, sourceItem) {
+            if (_.isArray(sourceItem)) {
+                return result && _.find(targetSet, function (targetItem) {
+                    return obj.isEqualSet(sourceItem,targetItem);
+                }) !== undefined;
+            } else if (_.isObject(sourceItem)) {
+                return result && _.find(targetSet, sourceItem) !== undefined;
+            } else {
+                return result && _.indexOf(targetSet, sourceItem) !== -1;
+            }
+        };
+
+        return _.reduce(set1, _.curry(traverseSet)(set2), true) &&
+                _.reduce(set2, _.curry(traverseSet)(set1), true);
+    };
+
+    /**
+     * Given a first set, return a subset containing all items which are not
+     * present in the second set.
+     * @param {Array} original set
+     * @param {Array} secondary to intersect with
+     * Examples:
+     *  findItemsNotInSet([1,2,3],[2,3]) -> [1]
+     *  findItemsNotInSet([1,2,3],[2,3,1]) -> []
+     *  findItemsNotInSet([1,{a:1},3],[3,1,{a:2}]) -> [{a:1}]
+     */
+    obj.findItemsNotInSet = function (set1, set2) {
+        return _.filter(set1, function (item1) {
+            return !_.find(set2, function (item2) {
+                return _.isEqual(item1,item2);
+            });
+        });
+    };
+
+    /**
      * Compares two objects and generates a patchset necessary to convert the second object to match the first
      * Examples:
      *   generatePatchSet({"a": 1, "b": 2}, {"a": 1}) returns:
@@ -135,6 +185,12 @@ define([
      *     {"operation":"add","field":"/a","value":1},
      *     {"operation":"add","field":"/b","value":2},
      *     {"operation":"remove","field":"/c"}
+     *   ]
+     *
+     *   generatePatchSet({"a": [1,2]}, {"a": [1,3]}) returns:
+     *   [
+     *     {"operation":"add","field":"/a/-","value":2},
+     *     {"operation":"remove","field":"/a","value":3}
      *   ]
      *
      * @param {object} newObject - the object to build up to
@@ -148,17 +204,44 @@ define([
             newValues = _.chain(newPointerMap)
                 .pairs()
                 .filter(function (p) {
-                    return previousPointerMap[p[0]] !== p[1];
+                    if (_.isArray(previousPointerMap[p[0]]) && _.isArray(p[1])) {
+                        return !obj.isEqualSet(previousPointerMap[p[0]], p[1]);
+                    } else {
+                        return !_.isEqual(previousPointerMap[p[0]], p[1]);
+                    }
                 })
                 .map(function (p) {
                     var finalPathToAdd = obj.walkDefinedPath(oldObjectClosure, p[0]),
-                        operation = (obj.getValueFromPointer(oldObjectClosure, p[0]) === undefined) ? "add" : "replace";
-                    return {
-                        "operation": operation,
-                        "field": finalPathToAdd,
-                        "value": obj.getValueFromPointer(newObjectClosure, finalPathToAdd)
-                    };
+                        newValueAtFinalPath = obj.getValueFromPointer(newObjectClosure, finalPathToAdd),
+                        oldValueAtFinalPath = obj.getValueFromPointer(oldObjectClosure, finalPathToAdd),
+                        setToPatchOperation = function (set, operation, path) {
+                            return _.map(set, function (item) {
+                                return {
+                                    "operation": operation,
+                                    "field": path,
+                                    "value": item
+                                };
+                            });
+                        };
+                    if (_.isArray(newValueAtFinalPath) && _.isArray(oldValueAtFinalPath)) {
+                        return setToPatchOperation(
+                                obj.findItemsNotInSet(newValueAtFinalPath, oldValueAtFinalPath),
+                                "add",
+                                finalPathToAdd + "/-" // add to set syntax
+                            ).concat(setToPatchOperation(
+                                obj.findItemsNotInSet(oldValueAtFinalPath, newValueAtFinalPath),
+                                "remove",
+                                finalPathToAdd
+                            ));
+                    } else {
+                        return {
+                            "operation": (oldValueAtFinalPath === undefined) ? "add" : "replace",
+                            "field": finalPathToAdd,
+                            "value": newValueAtFinalPath
+                        };
+                    }
                 })
+                .flatten()
                 // Filter out duplicates which might result from adding whole containers
                 // Have to stringify the patch operations to do object comparisons with uniq
                 .uniq(JSON.stringify)
--- forgerock-ui-commons/src/test/qunit/ObjectUtil.js
+++ forgerock-ui-commons/src/test/qunit/ObjectUtil.js
@@ -14,6 +14,8 @@
  * Copyright 2016 ForgeRock AS.
  */
 
+/*globals QUnit */
+
 define([
     "lodash",
     "org/forgerock/commons/ui/common/util/ObjectUtil"
@@ -22,17 +24,45 @@ define([
 
     QUnit.test("toJSONPointerMap", function () {
         var jsonMap = ObjectUtil.toJSONPointerMap({"c": 2, "a": {"b": ['x','y','z',true], "d": undefined }});
-        QUnit.equal(jsonMap["/a/b/2"], 'z', "toJSONPointerMap correctly flattens complex object");
+        QUnit.equal(jsonMap["/c"], '2', "toJSONPointerMap correctly flattens complex object");
+        QUnit.ok(_.isEqual(jsonMap["/a/b"], ['x','y','z',true]),
+            "toJSONPointerMap correctly returns a list when it encounters an array");
         QUnit.ok(!_.has(jsonMap, '/d'), "undefined value not included in map produced by toJSONPointerMap");
     });
 
     QUnit.test("getValueFromPointer", function () {
-        var testObject = {test:["apple", {"foo":"bar", "hello": "world"}]};
-        QUnit.equal(ObjectUtil.getValueFromPointer(testObject, "/test/0"), "apple", "/test/0");
-        QUnit.equal(ObjectUtil.getValueFromPointer(testObject, "/test/1/foo"), "bar", "/test/1/foo");
-        QUnit.ok(_.isEqual(ObjectUtil.getValueFromPointer(testObject, "/test/1"), {"foo":"bar", "hello": "world"}), "/test/1");
+        var testObject = {
+            testSet: ["apple", "pear"],
+            testMap: {"foo":"bar", "hello": "world"}
+        };
+        QUnit.equal(ObjectUtil.getValueFromPointer(testObject, "/testMap/foo"), "bar", "/testMap/foo");
+        QUnit.ok(
+            _.isEqual(ObjectUtil.getValueFromPointer(testObject, "/testSet"),
+            ["apple", "pear"],
+            "/testSet"));
         QUnit.equal(ObjectUtil.getValueFromPointer(testObject, "/test2"), undefined, "/test2");
         QUnit.equal(ObjectUtil.getValueFromPointer(testObject, "/"), testObject, "/");
+    });
+
+    QUnit.test("isEqualSet", function () {
+        QUnit.ok(ObjectUtil.isEqualSet([1], [1]), "Simple set equality");
+        QUnit.ok(!ObjectUtil.isEqualSet([1], [1,3]), "Simple set inequality");
+        QUnit.ok(ObjectUtil.isEqualSet([3,1], [1,3]), "Set equality regardless of order");
+        QUnit.ok(ObjectUtil.isEqualSet([3,{a:1},1], [1,3,{a:1}]), "Set equality with complex items");
+        QUnit.ok(!ObjectUtil.isEqualSet([3,{a:1},1], [1,3,{a:2}]),
+            "Set inequality with differing complex items");
+        QUnit.ok(ObjectUtil.isEqualSet([3,{a:1},['b','a'],1], [1,3,{a:1},['a','b']]),
+            "Set equality with complex objects, regardless of order, and with nested sets");
+    });
+
+    QUnit.test("findItemsNotInSet", function () {
+        QUnit.ok(_.isEqual(ObjectUtil.findItemsNotInSet([1,2,3],[2,3]), [1]), "Simple difference found");
+        QUnit.ok(_.isEqual(ObjectUtil.findItemsNotInSet([1,2,3],[2,3,1]), []),
+            "No differences found despite order differences");
+        QUnit.ok(_.isEqual(ObjectUtil.findItemsNotInSet([1,{a:1},3],[3,1,{a:2}]), [{a:1}]),
+            "Complex item difference recognized");
+        QUnit.ok(_.isEqual(ObjectUtil.findItemsNotInSet([1,{b:2,a:1},3],[3,1,{a:1,b:2}]), []),
+            "Complex item equality recognized, regardless of order");
     });
 
     QUnit.test("walkDefinedPath", function () {
@@ -42,35 +72,61 @@ define([
         QUnit.equal(ObjectUtil.walkDefinedPath(testObject, "/missing"), "/missing", "/missing");
         QUnit.equal(ObjectUtil.walkDefinedPath(testObject, "/missing/bar"), "/missing", "/missing/bar");
         QUnit.equal(ObjectUtil.walkDefinedPath({ } , "/foo"), "/foo", "/foo with empty object");
-        QUnit.equal(ObjectUtil.walkDefinedPath({ foo: undefined } , "/foo"), "/foo", "/foo as a property with undefined as the value");
-        QUnit.equal(ObjectUtil.walkDefinedPath({ foo: null } , "/foo/bar"), "/foo", "/foo as a property with null as the value");
-        QUnit.equal(ObjectUtil.walkDefinedPath({ foo: {bar:null} } , "/foo/bar"), "/foo/bar", "/foo/bar as a property with null as the value");
+        QUnit.equal(ObjectUtil.walkDefinedPath({ foo: undefined } , "/foo"),
+            "/foo",
+            "/foo as a property with undefined as the value");
+        QUnit.equal(ObjectUtil.walkDefinedPath({ foo: null }, "/foo/bar"),
+            "/foo",
+            "/foo as a property with null as the value");
+        QUnit.equal(ObjectUtil.walkDefinedPath({ foo: {bar:null} } , "/foo/bar"),
+            "/foo/bar",
+            "/foo/bar as a property with null as the value");
     });
 
     QUnit.test("generatePatchSet", function () {
         var patchDef = ObjectUtil.generatePatchSet({"a": 1, "b": 2}, {"a": 1});
-        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "add" && patchDef[0].field === "/b" && patchDef[0].value === 2,
+        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "add" &&
+                patchDef[0].field === "/b" && patchDef[0].value === 2,
             "Simple field addition returned for patchDef");
 
         patchDef = ObjectUtil.generatePatchSet({"a": 1, "b": 2}, {"c": 1});
-        QUnit.equal(patchDef.length, 3, "Expected operation count for removal of one attribute and addition of two others");
+        QUnit.equal(patchDef.length, 3,
+            "Expected operation count for removal of one attribute and addition of two others");
 
-        patchDef = ObjectUtil.generatePatchSet({"arrayItem": [undefined, {"sub": 2}]}, {"arrayItem": [{"sub": 1}, {"sub": 2}]});
-        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "remove" && patchDef[0].field === "/arrayItem/0",
-            "Removal of nested array value via explicit undefined removes whole array element");
+        patchDef = ObjectUtil.generatePatchSet({
+            "setItems": [{"sub": 2}]
+        }, {
+            "setItems": [{"sub": 1}, {"sub": 2}]
+        });
+        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "remove" &&
+                patchDef[0].field === "/setItems" && _.isEqual(patchDef[0].value, {"sub": 1}),
+            "Removal of value from set based on value of item");
+
+        /* note that the order of the items isn't relevant; only the content matters */
+        patchDef = ObjectUtil.generatePatchSet({
+            "setItems": [{"sub": 4}, {"sub": 2}, {"sub": 3}]
+        }, {
+            "setItems": [{"sub": 3}, {"sub": 2}]
+        });
+
+        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "add" &&
+                patchDef[0].field === "/setItems/-" && _.isEqual(patchDef[0].value, {"sub": 4}),
+            "Addition of value to set");
 
         patchDef = ObjectUtil.generatePatchSet({manager:{_ref: "a/b/c"}},{});
-        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "add" && patchDef[0].field === "/manager" && _.isEqual(patchDef[0].value, {_ref: "a/b/c"}),
+        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "add" &&
+                patchDef[0].field === "/manager" && _.isEqual(patchDef[0].value, {_ref: "a/b/c"}),
             "Addition of whole new complex property results in full map added");
 
         patchDef = ObjectUtil.generatePatchSet({manager:{_ref: "a/b/c"}},{manager:null});
-        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "replace" && patchDef[0].field === "/manager" && _.isEqual(patchDef[0].value, {_ref: "a/b/c"}),
+        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "replace" &&
+                patchDef[0].field === "/manager" && _.isEqual(patchDef[0].value, {_ref: "a/b/c"}),
             "Replacement of null value with whole new complex property results in full map added");
 
         patchDef = ObjectUtil.generatePatchSet({manager:{_ref: "a/b/d"}},{manager:{_ref: "a/b/c"}});
-        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "replace" && patchDef[0].field === "/manager/_ref" && patchDef[0].value === "a/b/d",
+        QUnit.ok(patchDef.length === 1 && patchDef[0].operation === "replace" &&
+                patchDef[0].field === "/manager/_ref" && patchDef[0].value === "a/b/d",
             "Replacement of simple value in nested map");
-
     });
 
 });
