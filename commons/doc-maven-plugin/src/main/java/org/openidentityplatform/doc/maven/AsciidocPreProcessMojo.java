f3a2dbfed4171f61d228905e25481f1a123f4ba4
maximthomas <maxim.thomas@gmail.com>
2025-03-07 13:42:49 +0300

    add version from GitHub to asciidoc

--- commons/doc-maven-plugin/src/main/java/org/openidentityplatform/doc/maven/AsciidocPreProcessMojo.java
+++ commons/doc-maven-plugin/src/main/java/org/openidentityplatform/doc/maven/AsciidocPreProcessMojo.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2024 3A Systems LLC.
+ * Copyright 2024-2025 3A Systems LLC.
  */
 
 package org.openidentityplatform.doc.maven;
@@ -19,6 +19,11 @@ package org.openidentityplatform.doc.maven;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -28,6 +33,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -35,6 +41,8 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -67,6 +75,10 @@ public class AsciidocPreProcessMojo extends AbstractAsciidocMojo {
             throw new MojoExecutionException("error copying partials content", e);
         }
 
+        copyDocsDirectories();
+    }
+
+    protected void copyDocsDirectories() throws MojoExecutionException {
         for(File docDir : getAsciidocSourceDirectory().listFiles()) {
             String document = FilenameUtils.getBaseName(docDir.toString());
 
@@ -80,6 +92,54 @@ public class AsciidocPreProcessMojo extends AbstractAsciidocMojo {
                 FileUtils.copyDirectory(docDir, new File(getSourceOutputDirectory(), document));
             } catch (IOException e) {
                 throw new MojoExecutionException("error copying " + document, e);
+            }
+        }
+    }
+
+    protected void updateVersionAttributes() throws MojoExecutionException, IOException {
+        String version = null;
+        String url = String.format("https://api.github.com/repos/OpenIdentityPlatform/%s/releases/latest", this.projectName);
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet(url);
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                System.out.println("Status Code: " + response.getStatusLine().getStatusCode());
+                String responseBody = EntityUtils.toString(response.getEntity());
+                System.out.println("Response Body: " + responseBody);
+                String pattern = "\"name\"\\s*:\\s*\"([^\"]+)\"";
+
+                // Compile the regex
+                Pattern regex = Pattern.compile(pattern);
+                Matcher matcher = regex.matcher(responseBody);
+
+                // Find and extract the value
+                if (matcher.find()) {
+                    version = matcher.group(1); // Group 1 contains the captured value
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(version == null) {
+            return;
+        }
+
+        setVersion(version);
+    }
+
+    protected void setVersion(String version) throws IOException {
+        String versionShort = version.replaceAll("(\\.\\d+)(?!.*\\d)", "");
+        for(File docDir : getSourceOutputDirectory().listFiles()) {
+            System.out.println(docDir);
+            for(File docFile : docDir.listFiles((dir, name) -> name.toLowerCase().endsWith(".adoc"))) {
+                System.out.println(docFile);
+                String adoc = FileUtils.readFileToString(docFile, StandardCharsets.UTF_8);
+                String versionRegex = String.format("(:%s-version:)\\s*(.*)\\b", this.projectName);
+                adoc = adoc.replaceAll(versionRegex, "$1" + version);
+
+                String versionShortRegex = String.format("(:%s-version-short:)\\s*(.*)\\b", this.projectName);
+                adoc = adoc.replaceAll(versionShortRegex, "$1" + versionShort);
+
+                FileUtils.writeStringToFile(docFile, adoc, StandardCharsets.UTF_8);
             }
         }
     }
