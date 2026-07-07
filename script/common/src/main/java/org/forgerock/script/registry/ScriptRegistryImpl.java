7b25122812ef7b215384bf1f014a68ae67f38f06
Valera V Harseko <vharseko@3a-systems.ru>
2026-07-07 18:45:44 +0300

    Release the write lock in ScriptRegistryImpl.removeSourceUnit (CodeQL java/unreleased-lock)
    removeSourceUnit(SourceContainer) acquired sourceCacheLock.writeLock()
    and, in the finally block, called writeLock().lock() again instead of
    unlock(). Each call therefore incremented the write hold count by two
    and released it zero times, permanently leaking the write lock and
    eventually deadlocking every reader and writer of sourceCache.
    
    Change the finally block to release the lock with writeLock().unlock().
    The lock is now correctly balanced with the acquisition on entry, as it
    already is in addSourceUnit.
    
    Fixes code-scanning alert #38.

--- script/common/src/main/java/org/forgerock/script/registry/ScriptRegistryImpl.java
+++ script/common/src/main/java/org/forgerock/script/registry/ScriptRegistryImpl.java
@@ -2,6 +2,7 @@
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
  * Copyright (c) 2013-2015 ForgeRock AS. All Rights Reserved
+ * Portions Copyrighted 2026 3A Systems, LLC
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -705,7 +706,7 @@ public class ScriptRegistryImpl implements ScriptRegistry, ScriptEngineFactoryOb
             try {
                 sourceCache.remove(unit);
             } finally {
-                sourceCacheLock.writeLock().lock();
+                sourceCacheLock.writeLock().unlock();
             }
 
             for (LibraryRecord cacheRecord : cache.values()) {
