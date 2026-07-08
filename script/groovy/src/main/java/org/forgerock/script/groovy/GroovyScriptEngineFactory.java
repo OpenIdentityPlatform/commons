fb292298a5c00a644d5f91a436a9466b7072d13f
Maxim Thomas <maxim.thomas@gmail.com>
2026-07-08 09:32:50 +0300

    Apply suggestions from code review
    Co-authored-by: Maxim Thomas <maxim.thomas@gmail.com>

--- script/groovy/src/main/java/org/forgerock/script/groovy/GroovyScriptEngineFactory.java
+++ script/groovy/src/main/java/org/forgerock/script/groovy/GroovyScriptEngineFactory.java
@@ -2,7 +2,6 @@
  * DO NOT REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
  * Copyright (c) 2012-2014 ForgeRock AS. All rights reserved.
- * Portions copyright 2026 3A Systems LLC.
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -21,6 +20,7 @@
  * with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
+ * Portions copyright 2026 3A Systems LLC.
  */
 
 package org.forgerock.script.groovy;
